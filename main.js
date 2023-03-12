46bede3fa3e23243487ff31f1ae5c9777ca0e489
Md Ibrahim <106436211+ibrahim0891@users.noreply.github.com>
2023-03-12 08:11:07 +0600

    Create

---
+++ main.js
@@ -0,0 +1,6 @@
+let inbox = document.getElementsByClassName('inbox')
+for (var i = 0; i < inbox.length; i++) {
+    inbox[i].addEventListener('click',()=>{
+        alert('Feature Not implemented.')
+    })
+}
